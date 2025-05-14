886a5430c55f51cb992bdf57fa761af8f86f4f83
Côme Chilliet <come.chilliet@nextcloud.com>
2025-05-14 17:13:27 +0200

    feat: Add login and getting apps/files route, better status checks
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- basic.js
+++ basic.js
@@ -4,7 +4,7 @@ import { group, check } from 'k6'
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.0.0/index.js'
 
 export let options = {
-  iterations: 100,
+  iterations: 20,
   vus: 10,
   duration: '10s',
 }
@@ -12,14 +12,39 @@ export let options = {
 export default function() {
   let response = http.request('GET', `${__ENV.BASEURI}/version.php`, undefined, { tags: { group: 'version.php' } })
   check(response, {
-    'status is 201 or 204': (r) => r.status === 201 || 204
+    'version.php status is >= 200 and < 300': (r) => r.status >= 200 && r.status < 300
   })
   response = http.request('GET', `${__ENV.BASEURI}/status.php`, undefined, { tags: { group: 'status.php' } })
   check(response, {
-    'status is 201 or 204': (r) => r.status === 201 || 204
+    'status.php status is >= 200 and < 300': (r) => r.status >= 200 && r.status < 300
   })
   response = http.request('GET', `${__ENV.BASEURI}/index.php`, undefined, { tags: { group: 'index.php' } })
   check(response, {
-    'status is 201 or 204': (r) => r.status === 201 || 204
+    'index.php status is >= 200 and < 300': (r) => r.status >= 200 && r.status < 300
   })
+
+  response = http.request('GET', `${__ENV.BASEURI}/index.php/csrftoken`)
+  let requestToken = response.json().token
+  response = http.post(
+    `${__ENV.BASEURI}/index.php/login`,
+    {
+      user: 'admin',
+      password: 'admin',
+      requesttoken: requestToken
+    }, {
+      headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // Add the Origin header so that the request is not blocked by the browser.
+      'Origin': `${__ENV.BASEURI}`, },
+  });
+  check(response, {
+    'login status is >= 200 and < 300': (r) => r.status >= 200 && r.status < 300
+  })
+
+  response = http.request('GET', `${__ENV.BASEURI}/index.php/apps/files/`, undefined, { tags: { group: 'apps/files' } })
+  check(response, {
+    'files status is >= 200 and < 300': (r) => r.status >= 200 && r.status < 300
+  })
+
+
 }
